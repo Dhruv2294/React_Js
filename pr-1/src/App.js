94bcbf50dcfa020eb11afc9107baebd180be9862
Dhruv2294 <131580275+Dhruv2294@users.noreply.github.com>
2023-11-04 13:44:08 +0530

    Project-1
    Project-1 (Circlet)

---
+++ pr-1/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AllQuestion from "./Component/AllQuestion";
+import ArrayOfobject from "./Component/ArryOfObj";
+
+function App() {
+  return (
+    <div className="App">
+      <AllQuestion />
+      <ArrayOfobject />
+    </div>
+  );
+}
+
+export default App;
