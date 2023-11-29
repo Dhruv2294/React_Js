296188c3112b1f2acfc6b021ca96151c1dea1fba
Dhruv2294 <131580275+Dhruv2294@users.noreply.github.com>
2023-11-29 17:43:26 +0530

    Project 3

---
+++ Project 3/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Question1 from './Component/Question1';
+
+function App() {
+  return (
+    <div className="App">
+      <Question1/>
+    </div>
+  );
+}
+
+export default App;
