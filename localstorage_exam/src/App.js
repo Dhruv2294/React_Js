dea4ee4d52e94f2d764325509de69a80e4179999
Dhruv2294 <131580275+Dhruv2294@users.noreply.github.com>
2024-02-20 12:17:12 +0530

    React Exam

---
+++ localstorage_exam/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Localstorage from './component/Localstorage';
+// import Localstorage from './component/localstorage';
+
+function App() {
+  return (
+    <div className="App">
+      <Localstorage/>
+    </div>
+  );
+}
+
+export default App;
