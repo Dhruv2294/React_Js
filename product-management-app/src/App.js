16114af22d39fd315210f406a99ba0a6edd4c7f3
Dhruv2294 <131580275+Dhruv2294@users.noreply.github.com>
2024-03-30 12:51:36 +0530

    React JS Exam

---
+++ product-management-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Productform from './componet/Productform';
+
+function App() {
+  return (
+    <div className="App">
+        < Productform/>
+    </div>
+  );
+}
+
+export default App;
